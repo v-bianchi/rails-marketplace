6bb5f78742708ab3076e901a045e267246a6961a
Claire Dupont <claire.dupont@kedgebs.com>
2018-02-26 15:07:19 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :products do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :requests, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
