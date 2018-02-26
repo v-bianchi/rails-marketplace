05395a6c6c8b133ebb66dc3056e1bc36d56cf525
Alexander Lomholt <arlomholt@gmail.com>
2018-02-26 15:45:37 -0500

    created products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/show'
+
+  get 'products/new'
+
+  get 'products/create'
+
+  get 'products/edit'
+
+  get 'products/update'
+
+  get 'products/destroy'
+
   devise_for :users
   root to: 'pages#home'
 
