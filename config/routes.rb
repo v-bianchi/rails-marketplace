2b7e3d22f524505cdc4bdf25418b80fd217fcdab
Alexander Lomholt <arlomholt@gmail.com>
2018-02-26 15:52:37 -0500

    deleted auto generated routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get 'products/index'
-
-  get 'products/show'
-
-  get 'products/new'
-
-  get 'products/create'
-
-  get 'products/edit'
-
-  get 'products/update'
-
-  get 'products/destroy'
-
   devise_for :users
   root to: 'pages#home'
 
