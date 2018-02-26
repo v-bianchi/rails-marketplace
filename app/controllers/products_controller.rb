c216a3a79edd285b6c7cdd2a6f8693aff1d0cd6b
Alexander Lomholt <arlomholt@gmail.com>
2018-02-26 16:24:30 -0500

    added 7 CRUD with product_params and before_action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,49 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [ :show, :edit, :update, :destroy ]
+
   def index
+    @products = Product.all
   end
 
   def show
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new
+    if @product.save
+      redirect_to @product
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @product.update
+      redirect_to @product
+    else
+      render :update
+    end
   end
 
   def destroy
+    @product.destroy
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :category, :picture, :description)
+  end
+
+  def find_product
+    @product.find(product_params)
+  end
+
 end
