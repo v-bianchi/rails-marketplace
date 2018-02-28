88f4dc97cd4ace7e710c837289b446664ead87e1
Claire Dupont <claire.dupont@kedgebs.com>
2018-02-28 12:14:08 -0500

    update pundit review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+<<<<<<< HEAD
+ protect_from_forgery with: :exception
+ before_action :authenticate_user!
 
-  before_action :authenticate_user!
-
-  include Pundit
-
+ include Pundit
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
@@ -12,13 +11,13 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(root_path)
-  end
+   flash[:alert] = "You are not authorized to perform this action."
+   redirect_to(root_path)
+ end
 
-  private
+ private
 
-  def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
-  end
-end
+ def skip_pundit?
+   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+ end
+
