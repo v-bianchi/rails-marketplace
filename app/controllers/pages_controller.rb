9728cd15aecfecfc0c5b42225f32d9627f7e1cf5
Victor Bianchi <vbianchi90@gmail.com>
2018-03-02 16:33:25 -0500

    fixed pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, except: (:index, :show)
+  skip_before_action :authenticate_user!, except: (:index)
   skip_after_action :verify_authorized
 
   def home
